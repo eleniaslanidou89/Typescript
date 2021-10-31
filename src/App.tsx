d860997ae8a86ccfb69eeaa7731ab6ef2e8ae463
Eleni <helena.aslanidou@gmail.com>
2021-10-31 10:57:15 +0100

    creted TodoList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import React, { useState } from "react";
 import InputField from "./components/InputField";
+import TodoList from "./components/TodoList";
 import "./App.css";
+import  { Todo } from "./models/models"
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
 
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if(todo) {
+      setTodos([...todos,{ id: Date.now(),isDone:false,todo:todo}])
+      setTodo("");
+    }
+  }
+  console.log(todos)
   return (
     <div className="App">
       <span className="heading">To Do</span>
-      <InputField todo={todo} setTodo={setTodo} />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 };
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Todo } from "../models/models";
+import "./styles.css"
+
+interface Props{
+    todos: Todo[];
+    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+const TodoList: React.FC<Props> = ({todos,setTodos}:Props) => {
+    return (
+        <div className="todos">
+             {todos.map((t) => (
+        <li>{t.todo}</li>
+      ))}
+        </div>
+    )
+}
+
+export default TodoList
